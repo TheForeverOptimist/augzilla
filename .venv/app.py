00f6b290fbc72824d4e21de6aac88368ebd3edbe
TheForeverOptimist <sean@elevate.app>
2023-07-06 15:47:15 -0400

    added an html file

--- .venv/app.py
+++ .venv/app.py
@@ -1,14 +1,7 @@
-from flask import Flask
-
-from markupsafe import escape
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Index Page'
-
-@app.route("/hello")
-def hello():
-    return 'Hello, World'
+def homepage():
+    return render_template('home.html')
