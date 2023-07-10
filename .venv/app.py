638f770e8799efea8a255986934c10072032bf53
TheForeverOptimist <sean@elevate.app>
2023-07-10 09:40:37 -0400

    Uploading dataset functionality created

--- .venv/app.py
+++ .venv/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+ALLOWED_EXTENSIONS = {'png', 'jpeg', 'jpg', 'tiff', 'heic', 'webp'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def homepage():
@@ -9,6 +15,32 @@ def homepage():
 @app.route('/begin.html')
 def begin():
     return render_template('begin.html')
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods =['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        #check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        #if user does not select a file, the browser submits an empty file without a file name
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+        return
+    
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 
 
